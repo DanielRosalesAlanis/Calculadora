fbe62c21f7eb3a14b4dc957b720c5db1b062db16
Daniel Rosales Alanis <20243ds001@utez.edu.mx~>
2025-11-18 10:33:11 -0600

    Primer commit

---
+++ calculadoravalidar/src/main/java/com/example/calculadoravalidar/CalculadoravalidarApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculadoravalidar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoravalidarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoravalidarApplication.class, args);
+	}
+
+}
---
+++ calculadoravalidar/src/test/java/com/example/calculadoravalidar/CalculadoravalidarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculadoravalidar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoravalidarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
